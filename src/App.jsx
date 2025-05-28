b71b3c93987ad85cc2890793a4f8c21c383985f0
teedev78 <i7891011t@gmail.com>
2025-05-28 16:23:55 +0700

    feat: do assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
 import "./App.css";
+import movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
       <section className="movie-list-section">
+        <h1>Movie List Section</h1>
         {/* Render Movie Lists Here */}
+        <ul>
+          {movies.map((movie, index) => {
+            return (
+              <li key={index}>
+                <div className="movie-image">
+                  <img
+                    src={movie.image}
+                    alt={movie.title}
+                    className="movie-thumbnail"
+                  />
+                </div>
+                <div className="movie-content">
+                  <div>Title: {movie.title}</div>
+                  <div>Year: {movie.year}</div>
+                  <div>Runtime: {movie.runtime}</div>
+                  <div className="genres">
+                    <div>Genres: </div>
+                    <div className="genres-list">
+                      {movie.genres.map((genre, index) => {
+                        return (
+                          <span key={index} className="genre">
+                            {genre}
+                          </span>
+                        );
+                      })}
+                    </div>
+                  </div>
+                  <div>IMDB Ratings: {movie.imdbRating}</div>
+                  <div>IMDB Votes: {movie.imdbVotes}</div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </section>
     </div>
   );
